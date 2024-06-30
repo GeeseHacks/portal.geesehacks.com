e9d3a91c8c996cc97cd2ba9d280121bd2e1f0ce3
Ri-Hong <riri.hong@gmail.com>
2024-06-29 21:26:16 -0400

    Fixed email authentication

--- frontend/app/api/auth/signin/route.ts
+++ frontend/app/api/auth/signin/route.ts
@@ -1,20 +1,20 @@
 // route: api/auth/signin
-import { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST(req: NextApiRequest) {
-  console.log("FINALLY");
-  const { email, password } = req.body;
+export async function POST(request: NextRequest) {
+  const body = await request.json();
 
-  if (email === "test@example.com" && password === "password123") {
+  const { provider, username, password } = body;
+
+  if (username === "test@example.com" && password === "password123") {
     // Successful authentication
-    return new NextResponse(JSON.stringify({ message: "Sign-in successful" }), 
-      {status: 200}
-  );
+    return new NextResponse(JSON.stringify({ message: "Sign-in successful" }), {
+      status: 200
+    });
   } else {
     // Failed authentication
-    return new NextResponse(JSON.stringify({ message: "CredentialsSignin" }), 
-      {status: 401}
-    );
+    return new NextResponse(JSON.stringify({ message: "Invalid Credentials" }), {
+      status: 401
+    });
   }
 }
