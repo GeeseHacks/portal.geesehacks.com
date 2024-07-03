7193a2a25faf8a0164b3e518500a64c6c9a4e3da
Ri-Hong <riri.hong@gmail.com>
2024-07-02 20:50:38 -0400

    Linked up signup functionality with database, added logout button

--- frontend/app/api/auth/users/[email]/route.ts
+++ frontend/app/api/auth/users/[email]/route.ts
@@ -2,14 +2,13 @@ import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@lib/prisma'; // Import the initialized Prisma client
 
 // Handler for GET requests
-export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+export async function GET(request: NextRequest, { params }: { params: { email: string } }) {
   try {
-    // Convert the 'id' parameter from string to integer
-    const userId = parseInt(params.id, 10);
+    const email = params.email;
 
     // Find the unique user by ID using Prisma
-    const user = await prisma.users.findUnique({
-      where: { id: userId },
+    const user = await prisma.user_auth.findUnique({
+      where: { email: email },
     });
 
     // If user is found, return the user data as JSON
@@ -17,7 +16,7 @@ export async function GET(request: NextRequest, { params }: { params: { id: stri
       return new NextResponse(JSON.stringify(user), { status: 200 });
     } else {
       // If user is not found, return a 404 error with a message
-      return new NextResponse(JSON.stringify({ error: 'User not found' }), { status: 404 });
+      return new NextResponse(JSON.stringify({ error: 'Email not found' }), { status: 404 });
     }
   } catch (error) {
     // Log any errors to the console
