339b7bf27e36d1616a89436106106e9d6017dd4f
Ri-Hong <riri.hong@gmail.com>
2024-07-03 19:59:42 -0400

    Added /api/auth/user to Swagger API docs, and moved CORS config to next.config.mjs

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,31 +1,3 @@
-// import { NextRequest, NextResponse } from 'next/server';
-
-// // CORS headers allowing only the Swagger UI origin
-// const corsHeaders = {
-//   'Access-Control-Allow-Credentials': 'true',
-//   'Access-Control-Allow-Origin': 'http://localhost:18512', // Default port of the Swagger UI server
-//   'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-//   'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-// };
-
-// export function middleware(request: NextRequest) {
-//     console.log('Middleware applied');
-//   if (request.method === 'OPTIONS') {
-//     return new NextResponse(null, { headers: corsHeaders });
-//   }
-
-//   const response = NextResponse.next();
-//   response.headers.set('Access-Control-Allow-Credentials', corsHeaders['Access-Control-Allow-Credentials']);
-//   response.headers.set('Access-Control-Allow-Origin', corsHeaders['Access-Control-Allow-Origin']);
-//   response.headers.set('Access-Control-Allow-Methods', corsHeaders['Access-Control-Allow-Methods']);
-//   response.headers.set('Access-Control-Allow-Headers', corsHeaders['Access-Control-Allow-Headers']);
-//   return response;
-// }
-
-// export const config = {
-//   matcher: '/api/:path*', // Apply middleware to all API routes
-// };
-
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
  
