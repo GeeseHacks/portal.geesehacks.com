d18a7d91536e3be091f9515873e6bd7987b83079
jenniferli8263 <75101197+jenniferli8263@users.noreply.github.com>
2024-07-15 14:01:58 -0400

    added optional fields, switched to fetch

--- components/apply.tsx
+++ components/apply.tsx
@@ -292,50 +292,73 @@ const RegistrationForm: React.FC = () => {
   const onSubmit: SubmitHandler<formSchemaType> = async (data) => {
     console.log("form submitted!");
     console.log(data);
-    
+
     try {
       if (!session?.user?.id) {
         throw new Error("User is not authenticated");
       }
 
-      const userId = session.user.id;
+      const userId = Number(session.user.id);
       const resumeFile = data.resume[0];
       const filename = encodeURIComponent(resumeFile.name);
 
       //Upload the resume file and get the URL
       const uploadResponse = await fetch(`/api/resume?filename=${filename}`, {
-        method: 'POST',
+        method: "POST",
         body: resumeFile,
       });
       const blob = await uploadResponse.json();
       const resumeUrl = blob.url;
 
-
-      const userData = await axios.post("/api/users", {
-        id: userId,
-        firstname: data.firstName,
-        lastname: data.lastName,
-        age: data.age,
-        email: data.email,
-        phone_number: data.phoneNumber,
-        school: data.school,
-        level_of_study: data.levelOfStudy,
-        field_of_study: data.fieldOfStudy,
-        country_of_residence: data.countryOfResidence,
-        address: data.address,
-        dietary_restrictions: data.dietaryRestrictions,
-        github: data.githubProfile,
-        linkedin: data.linkedin,
-        personal_website: data.personalWebsite,
-        resume: resumeUrl,  
+      const userData = await fetch("/api/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: userId,
+          firstname: data.firstName,
+          lastname: data.lastName,
+          age: data.age,
+          email: data.email,
+          phone_number: data.phoneNumber,
+          school: data.school,
+          level_of_study: data.levelOfStudy,
+          field_of_study: data.fieldOfStudy,
+          country_of_residence: data.countryOfResidence,
+          address: data.address,
+          dietary_restrictions: data.dietaryRestrictions,
+          github: data.githubProfile,
+          linkedin: data.linkedin,
+          personal_website: data.personalWebsite,
+          MLH_authorize: data.mlhCodeOfConduct && data.mlhPrivacyPolicy,
+          optional_consider: data.sexuality,
+          optional_gender: data.gender,
+          optional_pronouns: data.pronouns,
+          optional_race: data.ethnicity,
+          t_shirt_size: data.tShirtSize,
+          resume: resumeUrl,
+        }),
       });
       //const userId = userData.data.id;
 
-      await axios.post("/api/application-responses", {
-        userid: userId,
-        q1: data.q1,
-        q2: data.q2,
-        q3: data.q3,
+      // await axios.post("/api/application-responses", {
+      //   userid: userId,
+      //   q1: data.q1,
+      //   q2: data.q2,
+      //   q3: data.q3,
+      // });
+      const longAnswers = await fetch("/api/application-responses", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userid: userId,
+          q1: data.q1,
+          q2: data.q2,
+          q3: data.q3,
+        }),
       });
 
       reset(); //scuffed
