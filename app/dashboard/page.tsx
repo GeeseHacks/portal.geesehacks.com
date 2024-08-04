98a883f8cd39a747dcefd5a2117f3a3cfa5f1cb0
bennywu2019 <benny.wu.new@gmail.com>
2024-08-04 18:57:55 -0400

    feat: Add template Navbar component and fixed dashboard authentication issue

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/nav/Navbar";
+
+const Home: React.FC = () => {
+  return <div className="h-screen w-screen flex">
+    {/* Navbar */}
+    <Navbar />
+
+    {/* Home Content */}
+    I am page
+
+  </div>
+}
+export default Home;
