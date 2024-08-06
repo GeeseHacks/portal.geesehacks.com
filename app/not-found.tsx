2dbf8988aabbdd51e41e0016685dc434349a1b98
bennywu2019 <benny.wu.new@gmail.com>
2024-08-05 20:34:49 -0400

    chore: Update 404 page link to return home to /dashboard

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
     <div className="bg-cover h-screen flex flex-col justify-center items-center text-center" style={{ backgroundImage: "url('/static/images/background.png')"}}>
       <h2 className="text-5xl">404</h2>
       <h2 className="text m-3">Whoops, wrong page. Please leave before Mr. Goose finds out!</h2>
-      <Link className="underline" href="/">Return Home</Link>
+      <Link className="underline" href="/dashboard">Return Home</Link>
     </div>
   )
 }
