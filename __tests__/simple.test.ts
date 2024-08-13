b03b6a7c257122e191e148a8851eac1da849b163
Ri-Hong <riri.hong@gmail.com>
2024-08-13 18:37:45 -0400

    Init jest

---
+++ __tests__/simple.test.ts
@@ -0,0 +1,12 @@
+describe('Simple Test', () => {
+    it('should check if true is true', () => {
+      expect(true).toBe(true);
+    });
+  
+    it('should check if 1 + 1 equals 2', () => {
+      expect(1 + 1).toBe(3);
+    });
+  });
+  
+
+  
