e6650ebae09805c98bd583785a34c4b7d5fccbdb
jenniferli8263 <75101197+jenniferli8263@users.noreply.github.com>
2024-08-24 23:40:47 -0400

    added verify password and view password

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -1,17 +1,27 @@
 // pages/reset-password.tsx
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { validatePassword } from "@/lib/passwordUtils";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
+import { BiHide } from "react-icons/bi";
+import { BiShow } from "react-icons/bi";
 
 const ResetPassword = () => {
   const [newPassword, setNewPassword] = useState("");
   const [passwordErrors, setPasswordErrors] = useState<string[]>([]);
+  const [verifyPassword, setVerifyPassword] = useState("");
+  const [passwordVisibility, setPasswordVisibility] = useState({
+    password: false,
+    verifyPassword: false,
+  });
+  const [verifyPasswordError, setVerifyPasswordError] = useState<string>("");
+  const [isButtonActive, setIsButtonActive] = useState(false);
   const searchParams = useSearchParams();
   const router = useRouter();
 
@@ -24,17 +34,26 @@ const ResetPassword = () => {
   ) => {
     e.preventDefault();
 
-    const passwordErrors = validatePassword(newPassword);
+    const passwordValidationErrors = validatePassword(newPassword);
 
-    if (passwordErrors.length > 0) {
-      setPasswordErrors(passwordErrors);
+    if (passwordValidationErrors.length > 0) {
+      setPasswordErrors(passwordValidationErrors);
       return;
+    } else {
+      setPasswordErrors([]);
     }
 
     if (!token || typeof token !== "string") {
       console.error("Invalid token");
       return;
     }
+
+    if (newPassword !== verifyPassword) {
+      setVerifyPasswordError("Passwords do not match");
+      return;
+    }
+
+    setVerifyPasswordError("");
 
     const promise = fetch("/api/auth/reset-password", {
       method: "POST",
@@ -65,24 +84,90 @@ const ResetPassword = () => {
       });
   };
 
+  useEffect(() => {
+    const arePasswordsMatching = newPassword === verifyPassword;
+    setVerifyPasswordError(
+      arePasswordsMatching ? "" : "Passwords do not match"
+    );
+    setIsButtonActive(newPassword !== "" && arePasswordsMatching);
+  }, [newPassword, verifyPassword]);
+
   return (
     <div className="flex flex-col items-center justify-center gap-y-4">
       <h1 className="text-white text-4xl font-bold my-6">Reset Password</h1>
-      <Input
-        placeholder="Enter new password"
-        type="password"
-        className="max-w-xl mb-4"
-        value={newPassword}
-        onChange={(e) => setNewPassword(e.target.value)}
-      />
+      <Label htmlFor="password">Password</Label>
+      <div className="relative">
+        <Input
+          placeholder="Enter new password"
+          type={passwordVisibility.password ? "text" : "password"}
+          required
+          className="pr-11"
+          value={newPassword}
+          onChange={(e) => setNewPassword(e.target.value)}
+        />
+        <div
+          onClick={() =>
+            setPasswordVisibility((prev) => ({
+              ...prev,
+              password: !prev.password,
+            }))
+          }
+          className="absolute inset-y-0 right-3 flex items-center cursor-pointer"
+        >
+          {passwordVisibility.password ? (
+            <BiHide className="mr-1 text-xl" />
+          ) : (
+            <BiShow className="mr-1 text-xl" />
+          )}
+        </div>
+      </div>
+
       {passwordErrors.length > 0 && (
-        <ul className="text-red-500 text-sm list-disc list-inside">
+        <ul
+          role="alert"
+          className="text-red-500 text-sm mt-2 list-disc list-inside"
+        >
           {passwordErrors.map((error, index) => (
             <li key={index}>{error}</li>
           ))}
         </ul>
       )}
-      <Button onClick={handlePasswordChange}>Update Password</Button>
+      <Label htmlFor="verify-password">Verify Password</Label>
+      <div className="relative">
+        <Input
+          type={passwordVisibility.verifyPassword ? "text" : "password"}
+          id="verify-password"
+          name="verify-password"
+          placeholder="Verify your password"
+          required
+          value={verifyPassword}
+          onChange={(e) => setVerifyPassword(e.target.value)}
+          className="pr-11"
+        />
+        <div
+          onClick={() =>
+            setPasswordVisibility((prev) => ({
+              ...prev,
+              verifyPassword: !prev.verifyPassword,
+            }))
+          }
+          className="absolute inset-y-0 right-3 flex items-center cursor-pointer"
+        >
+          {passwordVisibility.verifyPassword ? (
+            <BiHide className="mr-1 text-xl" />
+          ) : (
+            <BiShow className="mr-1 text-xl" />
+          )}
+        </div>
+      </div>
+      {verifyPasswordError && (
+        <p role="alert" className="text-red-500 text-sm mt-2">
+          {verifyPasswordError}
+        </p>
+      )}
+      <Button onClick={handlePasswordChange} disabled={!isButtonActive}>
+        Update Password
+      </Button>
     </div>
   );
 };
