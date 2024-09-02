0e50637df5f5e6d4766faf346ca6a876073f4dc0
jenniferli8263 <75101197+jenniferli8263@users.noreply.github.com>
2024-09-02 16:37:30 -0400

    it's so over

--- next.config.mjs
+++ next.config.mjs
@@ -15,9 +15,10 @@ const nextConfig = {
             }
         ]
     },
-    webpack: (config, { isServer }) => {
-        console.log('Config NEXT_PUBLIC_VERCEL_URL:', process.env.NEXT_PUBLIC_VERCEL_URL);
-        return config;
+    publicRuntimeConfig: {
+        NEXT_PUBLIC_VERCEL_URL: process.env.VERCEL_URL
+            ? `https://${process.env.VERCEL_URL}`
+            : 'http://localhost:3000', // Fallback for local development
     },
 }
 export default nextConfig;
