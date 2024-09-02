152e6cda52e3564af28e8c637b851b91cc49f14e
bennywu2019 <benny.wu.new@gmail.com>
2024-09-02 18:19:22 -0400

    cleanup: dashboard bug fixes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import SideNav from "@/components/nav/SideNav";
 import { Filter } from "lucide-react";
 import Head from "next/head";
@@ -5,30 +6,30 @@ import Head from "next/head";
 const Home: React.FC = () => {
   return (
     <div className="h-screen w-screen flex overflow-y-scroll">
-      {/* Glow Effect */}
-      <div className="absolute inset-0 z-0">
-        <div className="absolute top-48 left-48 m-20 w-1/2 h-1/2 bg-purple-700 rounded-full blur-4xl opacity-20"></div>
-        <div className="absolute bottom-48 right-48 m-20 w-1/2 h-1/2 bg-teal-700 rounded-full blur-4xl opacity-20"></div>
-      </div>
+      {/* Glow Effect (Currently extremely laggy performance on Safari due to "4xl" */}
+      {/* <div className="absolute inset-0 -z-50">
+        <div className="absolute top-48 left-48 m-20 w-1/4 h-1/4 bg-purple-700 rounded-full blur-3xl opacity-20"></div>
+        <div className="absolute bottom-48 right-48 m-20 w-1/4 h-1/4 bg-teal-700 rounded-full blur-3xl opacity-5"></div>
+      </div> */}
 
-      <div className="absolute inset-0 z-0">
-      {[...Array(400)].map((_, i) => {
-        const size = `${Math.random() + 1}px`;
-        return (
-          <div
-            key={i}
-            className="absolute bg-white rounded-full opacity-35"
-            style={{
-              width: size,
-              height: size,
-              top: `${Math.random() * 100}%`,
-              left: `${Math.random() * 100}%`,
-              boxShadow: `0 0 10px 2px rgba(255, 255, 255, 0.1)`,
-            }}
-          ></div>
-        );
-      })}
-    </div>
+      <div className="absolute inset-0 -z-50">
+        {[...Array(120)].map((_, i) => { // Reduced from 100 to 50
+          const size = `${Math.random() + 1}px`;
+          return (
+            <div
+              key={i}
+              className="absolute bg-white rounded-full opacity-35"
+              style={{
+                width: size,
+                height: size,
+                top: `${Math.random() * 100}%`,
+                left: `${Math.random() * 100}%`,
+                boxShadow: `0 0 10px 2px rgba(255, 255, 255, 0.1)`,
+              }}
+            ></div>
+          );
+        })}
+      </div>
 
       {/* SideNav */}
       <SideNav />
@@ -44,8 +45,8 @@ const Home: React.FC = () => {
           bg-gradient-to-r from-darkpurple to-darkteal 
           p-12 rounded-xl w-full min-h-52 space-y-4 
           relative overflow-hidden 
-          hover:scale-102 transition-transform duration-300
-          drop-shadow-[0_0px_15px_rgba(48,133,159,0.5)]
+          hover:scale-102 transition-transform duration-300 
+          hover:drop-shadow-[0_0px_15px_rgba(48,133,159,0.5)]
         ">
           <img src="/static/images/status-notsubmitted.png" alt="Not Submitted" className="absolute right-48 -top-10 z-0" />
           <h2 className="font-light text-lg drop-shadow-[0_0px_5px_rgba(0,0,0,0.5)]">Application Status</h2>
@@ -58,18 +59,18 @@ const Home: React.FC = () => {
             bg-gradient-to-r from-darkpurple to-darkteal 
             p-12 rounded-xl
             relative overflow-hidden 
-            hover:scale-102 transition-transform duration-300
-            drop-shadow-[0_0px_15px_rgba(48,133,159,0.5)]
+            hover:scale-102 transition-transform duration-300 
+            hover:drop-shadow-[0_0px_15px_rgba(48,133,159,0.5)]
           ">
-            <img src="/static/images/faq.png" alt="FAQ" className=" absolute -right-10 bottom-0 z-0" />
+            <img src="/static/images/faq.png" alt="FAQ" className="absolute -right-10 bottom-0 z-0" /> 
             <h2 className="font-semibold text-4xl">FAQ</h2>
           </div>
           <div className="
             bg-gradient-to-r from-darkpurple to-darkteal 
             p-12 rounded-xl
             relative overflow-hidden 
-            hover:scale-102 transition-transform duration-300
-            drop-shadow-[0_0px_15px_rgba(48,133,159,0.5)]
+            hover:scale-102 transition-transform duration-800
+            hover:drop-shadow-[0_0px_15px_rgba(48,133,159,0.5)]
           ">
             <h2 className="font-semibold text-4xl">QR Code</h2>
           </div>
