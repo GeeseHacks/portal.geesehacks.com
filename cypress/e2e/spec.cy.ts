46089163fd00526ffbb20a8881cfff8c8c63ff85
Ri-Hong <riri.hong@gmail.com>
2024-09-06 18:22:45 -0400

    Attempting to add cypress to GHA pipeline

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,9 +1,10 @@
 describe('template spec', () => {
   it('logs in with the test email and password', () => {
-    cy.visit('http://localhost:3000/login')
-
+    const deploymentUrl = Cypress.env('DEPLOYMENT_URL');
     const testEmail = Cypress.env('TEST_EMAIL')
     const testPassword = Cypress.env('TEST_PASSWORD')
+    
+    cy.visit(`${deploymentUrl}/login`);
 
     // Ensure the page loads properly and the email input is visible
     cy.get('input[name="email"]').type(testEmail)
