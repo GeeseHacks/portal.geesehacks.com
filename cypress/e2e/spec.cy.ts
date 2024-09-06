07c767103b926aa6143d39d4b6d6b2549b9e29cb
Ri-Hong <riri.hong@gmail.com>
2024-09-06 18:54:44 -0400

    Switched to using baseUrl

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,10 +1,9 @@
 describe('template spec', () => {
   it('logs in with the test email and password', () => {
-    const deploymentUrl = Cypress.env('DEPLOYMENT_URL');
     const testEmail = Cypress.env('TEST_EMAIL')
     const testPassword = Cypress.env('TEST_PASSWORD')
     
-    cy.visit(`${deploymentUrl}/login`);
+    cy.visit(`/login`);
 
     // Ensure the page loads properly and the email input is visible
     cy.get('input[name="email"]').type(testEmail)
