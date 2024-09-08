16fb0b7c1da7e63adb42a91644caca05f05adf13
jenniferli8263 <75101197+jenniferli8263@users.noreply.github.com>
2024-09-08 18:18:37 -0400

    init leaderboard

--- app/dashboard/stock-market/StockMarket.tsx
+++ app/dashboard/stock-market/StockMarket.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import SideNav from '../../../components/nav/SideNav'; 
+import React from "react";
+import SideNav from "../../../components/nav/SideNav";
 
 const StockMarket: React.FC = () => {
   return (
