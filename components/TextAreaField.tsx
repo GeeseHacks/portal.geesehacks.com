8b359fc2fa980719a84c6fe54bc0230d43338286
deekshuk12 <deekshu.kannan.d@gmail.com>
2024-09-22 22:39:58 -0400

    fixed conflicts

--- components/TextAreaField.tsx
+++ components/TextAreaField.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import { UseFormRegisterReturn, FieldError } from "react-hook-form";
-import { Textarea } from "@/components/ui/textarea";
+import { Textarea } from "@/components/ui/textarea"
 
 interface TextAreaFieldProps {
   id: string;
@@ -8,7 +8,7 @@ interface TextAreaFieldProps {
   registerOptions: UseFormRegisterReturn;
   placeholder?: string;
   error?: FieldError | undefined;
-  maxLength?: number; // optional prop to define max character length
+  maxLength: 1000;
 }
 
 const TextAreaField: React.FC<TextAreaFieldProps> = ({
@@ -17,9 +17,7 @@ const TextAreaField: React.FC<TextAreaFieldProps> = ({
   registerOptions,
   placeholder,
   error,
-  maxLength = 1000, // default max length
-}) => {
-  const [charCount, setCharCount] = useState(0);
+}) => (
   <div className="col-span-1">
     <label className="block mb-2 text-xl font-semibold" htmlFor={id}>
       {label}
@@ -34,30 +32,5 @@ const TextAreaField: React.FC<TextAreaFieldProps> = ({
     {error && <p role="alert" className="text-red-500 text-s italic mt-2">{error.message}</p>}
   </div>
 );
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setCharCount(e.target.value.length);
-  };
-
-  return (
-    <div className="col-span-1">
-      <label className="block mb-2 text-xl font-semibold" htmlFor={id}>
-        {label}
-      </label>
-      <Textarea
-        id={id}
-        {...registerOptions}
-        onChange={handleInputChange}
-        className={`mt-4 h-48 focus:border-transparent`}
-        placeholder={placeholder}
-        maxLength={maxLength} // optional maxLength attribute
-      />
-      <div className="text-right text-sm mt-1">
-        {charCount}/{maxLength} characters
-      </div>
-      {error && <p className="text-red-500 text-s italic mt-2">{error.message}</p>}
-    </div>
-  );
-};
 
 export default TextAreaField;
