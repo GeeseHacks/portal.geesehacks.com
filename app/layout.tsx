d3344e9ce57166fc4b1197e93de1167efe486d1f
icsbillwei <81921463+icsbillwei@users.noreply.github.com>
2025-01-24 15:12:31 -0500

    font -> next font pls work

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,10 +20,10 @@ export default async function RootLayout({
       <head>
         <meta name="description" content="GeeseHacks, a hybrid hackathon and case competition, will be held from January 25-26, 2025. Participants are invited to create innovative projects or craft unique solutions for real-world challenges. Join us for a weekend of creativity, collaboration, and problem-solving!" />
         <link rel="icon" href="/favicon.ico" />
-        <link
+        {/* <link
           href="https://fonts.googleapis.com/css2?family=Just+Another+Hand&display=swap"
           rel="stylesheet"
-        />
+        /> */}
 
       </head>
       <body className={inter.className}>
